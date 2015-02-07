3ac2560213033c5fb7f9fe6d9b980acef26bc82d
zeitsern <xxxthats4xes@gmail.com>
2015-02-07 16:18:34 -0500

    Complete redesign of board
    No 2D arrays needed anymore

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-var loc = new Array(14);
-var piece = new Array(14);
+var piece = new Array(196);
+var lights = new Array(190);
 var turn = 0; //4 players: 0,1,2,3
 var selecting = 0;
 var currentPiece = 0;
 var oldi = 0;
 for(i=0; i<14; i++)
 {
-	loc[i] = new Array(14);
 	piece[i] = new Array(14);
 }
 
@@ -14,6 +13,8 @@ $(document).ready(function() {
 	var turn = 0; //4 players: 0,1,2,3
 	var selecting = 0;
 	var currentPiece = 0;
+	piece = [0, 0, 0, 2, 3, 4, 6, 5, 4, 3, 2, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 3, 4, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 4, 6, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 5, 5, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 6, 4, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 4, 3, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 3, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 2, 3, 4, 5, 6, 4, 3, 2, 0, 0, 0];
+
 	for(i = 0; i < 196; i++) {
 		$('#container').append('<div id="box"><div id="box' + i + '">' + i + '</div></div>');
 		$('#box' + i).css( "width", "40px");
@@ -23,27 +24,8 @@ $(document).ready(function() {
 			$('#box' + i).css("backgroundColor", "black");
 			$('#box' + i).html(".");
 		}
-		loc[i%14][i/14] = i%14 + "," + parseInt(i/14);
-		piece[i%14][i/14] = "0";
-		$('#box' + i).html(i + "=" + piece[i%14][i/14] + "<br />" + loc[i%14][i/14]);
+		$('#box' + i).html(i + "<br />" + piece[i]);
 	}
-	piece = [
-	[0, 0, 0, 2, 3, 4, 6, 5, 4, 3, 2, 0, 0, 0],
-	[0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2],
-	[3, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 3],
-	[4, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 4],
-	[6, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 5],
-	[5, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 6],
-	[4, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 4],
-	[3, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 3],
-	[2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
-	[0, 0, 0, 2, 3, 4, 5, 6, 4, 3, 2, 0, 0, 0]
-	];
-	
 	for(i=0; i<196; i++)
 	{
 		$("#box" + i).click(boxSelected(i));
@@ -56,7 +38,6 @@ $(document).ready(function() {
 		$('#box' + i).css( "border-top", "1px solid black");
 	}
 	for(i = 0; i < 196; i++) {
-		$('#box' + i).html(i + "=" + piece[i%14][parseInt(i/14)] + "<br />" + loc[i%14][i/14]);
 		if(((i%14 < 3 || i%14 > 10) && i < 42) || ((i%14 < 3 || i%14 > 10) && i > 153))
 		{
 			$('#box' + i).css("backgroundColor", "black");
@@ -67,20 +48,20 @@ $(document).ready(function() {
 
 function boxSelected( i ){
   return function(){
-	if(turn < 5 && selecting == 0 && piece[i%14][parseInt(i/14)] != 0) //turn < 5 for now, anyone can play as anyone.
+	if(turn < 5 && selecting == 0 && piece[i] != 0) //turn < 5 for now, anyone can play as anyone.
 	{
 		$("#box" + i).css("backgroundColor", "orange");
-		currentPiece = piece[i%14][parseInt(i/14)];
+		currentPiece = piece[i];
 		oldi = i;
 		selecting = 1;
 		lightPossibles( i );
 	}
 	else if(turn < 4 && selecting == 1)
 	{
-		piece[oldi%14][parseInt(oldi/14)] = 0;
-		piece[i%14][parseInt(i/14)] = currentPiece;
-		$('#box' + oldi).html(oldi + "=0<br />" + loc[i%14][i/14]); //Reset origional piece
-		$('#box' + i).html(i + "=" + currentPiece + "<br />" + loc[i%14][i/14]);
+		piece[oldi] = 0;
+		piece[i] = currentPiece;
+		$('#box' + oldi).html(oldi + "<br />0"); //Reset original piece
+		$('#box' + i).html(i + "<br />" + currentPiece);
 		$("#box" + oldi).css("backgroundColor", "initial");
 		undoLights( oldi, currentPiece);
 		selecting = 0;
@@ -92,17 +73,23 @@ function boxSelected( i ){
 }
 
 function lightPossibles( i ){
-	if( piece[i%14][parseInt(i/14)] == 1)
+	if( piece[i] == 1)
 	{
 		$("#box" + (i-14)).css("backgroundColor", "yellow");
+		lights[0] = i-14;
 		$("#box" + (i-28)).css("backgroundColor", "yellow");
+		lights[1] = i-28;
 	}
 }
 
-function undoLights( oldi, currentPiece ){
-	if( currentPiece == 1)
+function undoLights(){
+	/*if( currentPiece == 1)
 	{
 		$("#box" + (oldi-14)).css("backgroundColor", "initial");
 		$("#box" + (oldi-28)).css("backgroundColor", "initial");
+	}*/
+	for(i=0;i<lights.length;i++)
+	{
+		$("#box" + lights[i]).css("backgroundColor", "initial");
 	}
 }
