d563006bf8faadae1c30dcb61cd1ba71c6c8d20e
zeitsern <xxxthats4xes@gmail.com>
2015-02-07 14:12:27 -0500

    Baisc movement of pawns working
    two new functions: lightPossibles(i) highlight possible moves and
    undoLights(oldi,currentPiece) take it away.

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 var loc = new Array(14);
 var piece = new Array(14);
+var turn = 0; //4 players: 0,1,2,3
+var selecting = 0;
+var currentPiece = 0;
+var oldi = 0;
 for(i=0; i<14; i++)
 {
 	loc[i] = new Array(14);
@@ -7,6 +11,9 @@ for(i=0; i<14; i++)
 }
 
 $(document).ready(function() {
+	var turn = 0; //4 players: 0,1,2,3
+	var selecting = 0;
+	var currentPiece = 0;
 	for(i = 0; i < 196; i++) {
 		$('#container').append('<div id="box"><div id="box' + i + '">' + i + '</div></div>');
 		$('#box' + i).css( "width", "40px");
@@ -39,7 +46,7 @@ $(document).ready(function() {
 	
 	for(i=0; i<196; i++)
 	{
-		$("#box" + i).click(createCallback(i));
+		$("#box" + i).click(boxSelected(i));
 	}
 	
 	for(i=0; i<196; i=i+14) {
@@ -58,8 +65,44 @@ $(document).ready(function() {
 	}
 });
 
-function createCallback( i ){
+function boxSelected( i ){
   return function(){
-    $("#box" + i).css("backgroundColor", "yellow");
+	if(turn < 5 && selecting == 0 && piece[i%14][parseInt(i/14)] != 0) //turn < 5 for now, anyone can play as anyone.
+	{
+		$("#box" + i).css("backgroundColor", "orange");
+		currentPiece = piece[i%14][parseInt(i/14)];
+		oldi = i;
+		selecting = 1;
+		lightPossibles( i );
+	}
+	else if(turn < 4 && selecting == 1)
+	{
+		piece[oldi%14][parseInt(oldi/14)] = 0;
+		piece[i%14][parseInt(i/14)] = currentPiece;
+		$('#box' + oldi).html(oldi + "=0<br />" + loc[i%14][i/14]); //Reset origional piece
+		$('#box' + i).html(i + "=" + currentPiece + "<br />" + loc[i%14][i/14]);
+		$("#box" + oldi).css("backgroundColor", "initial");
+		undoLights( oldi, currentPiece);
+		selecting = 0;
+		turn ++;
+	
+	if(turn > 3){turn=0;}
+	}
   }
+}
+
+function lightPossibles( i ){
+	if( piece[i%14][parseInt(i/14)] == 1)
+	{
+		$("#box" + (i-14)).css("backgroundColor", "yellow");
+		$("#box" + (i-28)).css("backgroundColor", "yellow");
+	}
+}
+
+function undoLights( oldi, currentPiece ){
+	if( currentPiece == 1)
+	{
+		$("#box" + (oldi-14)).css("backgroundColor", "initial");
+		$("#box" + (oldi-28)).css("backgroundColor", "initial");
+	}
 }
