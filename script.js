f0d0417df833bd6ef1f527e642d8eab5e8a5538d
zeitsern <xxxthats4xes@gmail.com>
2015-02-08 20:14:05 -0500

    Images and box ownership added
    The owner[] array stores the player numbers that owns the box. The whole
    game will be played from player 1's perspective, all other players will
    be converted server-side.

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 var piece = new Array(196);
+var owner = new Array(196);
 var lights = new Array(190);
-var turn = 0; //4 players: 0,1,2,3
+var turn = 1; //4 players: 1,2,3,4
 var selecting = 0;
 var currentPiece = 0;
+var currentOwner = 0;
 var oldi = 0;
 for(i=0; i<14; i++)
 {
@@ -14,27 +16,29 @@ $(document).ready(function() {
 	var selecting = 0;
 	var currentPiece = 0;
 	piece = [0, 0, 0, 2, 3, 4, 6, 5, 4, 3, 2, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 3, 4, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 4, 6, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 5, 5, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 6, 4, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 4, 3, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 3, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 2, 3, 4, 5, 6, 4, 3, 2, 0, 0, 0];
-
+	owner = [0, 0, 0, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0];
+	
 	for(i = 0; i < 196; i++) {
 		if(((i%14 < 3 || i%14 > 10) && i < 42) || ((i%14 < 3 || i%14 > 10) && i > 153))
 		{
 			$('#container').append('<div id="box"><div id="blackbox' + i + '">' + i + '</div></div>');
-			$('#blackbox' + i).css( "width", "40px");
-			$('#blackbox' + i).css( "height", "40px");
+			$('#blackbox' + i).css( "width", "45px");
+			$('#blackbox' + i).css( "height", "45px");
 			$('#blackbox' + i).css("backgroundColor", "black");
 		}
 		else
 		{
 		$('#container').append('<div id="box"><div id="box' + i + '">' + i + '</div></div>');
 		}
-		$('#box' + i).css( "width", "40px");
-		$('#box' + i).css( "height", "40px");
+		$('#box' + i).css( "width", "45px");
+		$('#box' + i).css( "height", "45px");
 		/*{ //This was the old method to add the black boxes
 			$('#box' + i).css("backgroundColor", "black");
 			$('#box' + i).html("<br \>");
 		}*/
-		$('#box' + i).html(i + "<br />" + piece[i]);
-		if(piece[i] == 2){$('#box' + i).html("<img src='images/2.png'></img>");}
+		$('#box' + i).html(i + "<br />" + piece[i] + "," + owner[i]);
+		if(piece[i] > 0){$('#box' + i).html("<img src='images/" + owner[i] + piece[i] + ".png'></img>");}
+		else{$('#box' + i).html("");}
 		$('#blackbox' + i).html(i + "<br />0");
 	}
 	for(i=0; i<196; i++)
@@ -54,10 +58,11 @@ $(document).ready(function() {
 
 function boxSelected( i ){
   return function(){
-	if(turn < 5 && selecting == 0 && piece[i] != 0) //turn < 5 for now, anyone can play as anyone.
+	if(turn == 1 && selecting == 0 && piece[i] != 0 && owner[i] == 1) //playing as player 1
 	{
 		$("#box" + i).css("backgroundColor", "orange");
 		currentPiece = piece[i];
+		currentOwner = owner[i];
 		oldi = i;
 		selecting = 1;
 		lightPossibles( i );
@@ -65,13 +70,14 @@ function boxSelected( i ){
 	else if(turn < 4 && selecting == 1)
 	{
 		piece[oldi] = 0;
+		owner[oldi] = 0;
 		piece[i] = currentPiece;
-		$('#box' + oldi).html(oldi + "<br />0"); //Reset original piece
-		if(currentPiece==2){$('#box' + i).html("<img src='images/2.png'></img>");} else{$('#box' + i).html(i + "<br />" + currentPiece);}
+		owner[i] = currentOwner;
+		$('#box' + oldi).html(""); //Reset original piece
+		$('#box' + i).html("<img src='images/" + owner[i] + piece[i] + ".png'></img>");
 		$("#box" + oldi).css("backgroundColor", "initial");
 		undoLights();
 		selecting = 0;
-		turn ++;
 	if(turn > 3){turn=0;}
 	}
   }
@@ -93,7 +99,7 @@ function lightPossibles( i ){
 	{
 		for(c=1;c<14;c++)
 		{
-			if((i+c)%14==0) {break;}
+			if((i+c)%14==0 || owner[i+c] == 1) {break;}
 			if( piece[i+c] == 0 )
 			{
 				$("#box" + (i+c)).css("backgroundColor", "yellow");
@@ -108,7 +114,7 @@ function lightPossibles( i ){
 		}
 		for(c=1;c<14;c++)
 		{
-			if((i-c)%14==13) {break;}
+			if((i-c)%14==13 || owner[i-c] == 1) {break;}
 			if( piece[i-c] == 0 )
 			{
 				$("#box" + (i-c)).css("backgroundColor", "yellow");
@@ -123,6 +129,7 @@ function lightPossibles( i ){
 		}
 		for(c=1;c<14;c++)
 		{
+			if(i+(14*c)>196 || owner[i+(14*c)] == 1) {break;}
 			if( piece[i+(14*c)] == 0 )
 			{
 				$("#box" + (i+(14*c))).css("backgroundColor", "yellow");
@@ -137,6 +144,7 @@ function lightPossibles( i ){
 		}
 		for(c=1;c<14;c++)
 		{
+			if(i-(14*c)<0 || owner[i-(14*c)] == 1) {break;}
 			if( piece[i-(14*c)] == 0 )
 			{
 				$("#box" + (i-(14*c))).css("backgroundColor", "yellow");
